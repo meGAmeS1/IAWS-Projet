a9f615ecedcefa9259c83bb179fc2e516b94bb95
meGAmeS1 <florentin.lemoal@gmail.com>
2015-03-23 17:39:17 +0100

    Première version de l'API pour l'exigence E1

--- UGmont/src/main/java/UGmont/MyResource.java
+++ UGmont/src/main/java/UGmont/MyResource.java
@@ -3,13 +3,18 @@ package UGmont;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
 /**
  * Root resource (exposed at "myresource" path)
  */
-@Path("myresource")
+@Path("api")
 public class MyResource {
+    private static final String API_URL = "http://www.omdbapi.com/?";
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -18,8 +23,26 @@ public class MyResource {
      * @return String that will be returned as a text/plain response.
      */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Path("films")
+    @Produces(MediaType.APPLICATION_XML)
+    public String getFilms(@QueryParam("titre") String titre, @QueryParam("annee") String annee) {
+        if (titre == null) {
+            return "<root response=\"False\"><error>Empty query</error></root>";
+        } else {
+            Client c = ClientBuilder.newClient();
+            WebTarget wt = c.target(API_URL).queryParam("r", "xml").queryParam("s", titre);
+
+            if (annee != null) {
+                wt = wt.queryParam("y", annee);
+            }
+
+            return wt.request(MediaType.APPLICATION_XML).get(String.class);
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
     public String getIt() {
-        return "Got it!";
+        return "<root response=\"False\"><error>No method called</error></root>";
     }
 }
